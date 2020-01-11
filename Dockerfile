ed439885f419fee3ece42356758b9d3ed627b69b
grandmacks <grandmacks@gmail.com>
2020-01-11 21:58:14 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM node:10.13-alpine
 WORKDIR /app
 ADD index.js  package.json  yarn.lock /app/
 RUN yarn install
+EXPOSE 8080
 CMD ["node","index.js"]
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(port, function() {
-  console.log('Example app listening on port ' + port);
+app.listen(8080, function() {
+  console.log('Example app listening on port 8080');
 });
